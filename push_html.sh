d9eed2635a5f0a9db1cb9f1be1faec2bdd819488
nathaniellao <lao.nathan@yahoo.com>
2018-04-05 14:14:23 -0400

    Tidied up the document will try to extract data from billboard top 100

--- push_html.sh
+++ push_html.sh
@@ -5,5 +5,9 @@ dst="/Users/NateLao/google_drive/Website/naterjlao.github.io"
 src="./*.html"
 
 cp $src $dst
+cd $dst
+git add -A
+git commit -m 'copied html to current directory'
+git push
 
 echo "Copied $src to $dst"
